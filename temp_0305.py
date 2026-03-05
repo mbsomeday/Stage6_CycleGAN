1f54eb8eb48329c67268f232d68392196ed15a88
mbsomeday <137542994@qq.com>
2026-03-05 11:21:34 +0000

    update

--- temp_0305.py
+++ temp_0305.py
@@ -1,24 +1,19 @@
-
 import os
+import torch
+import torch.distributed as dist
 
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+rank = int(os.environ.get("RANK", 0))
+local_rank = int(os.environ.get("LOCAL_RANK", 0))
 
-is_ddp = "WORLD_SIZE" in os.environ and int(os.environ["WORLD_SIZE"]) > 1
+is_ddp = world_size > 1
+print(f"is_ddp={is_ddp}, world_size={world_size}, rank={rank}, local_rank={local_rank}")
+
+if torch.cuda.is_available():
+    torch.cuda.set_device(local_rank)
+    print("using", torch.cuda.current_device(), torch.cuda.get_device_name(torch.cuda.current_device()))
+
 if is_ddp:
-    print('current is in ddp mode', os.environ["WORLD_SIZE"])
-else:
-    print('no ddp!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    dist.init_process_group(backend="nccl")
+    dist.barrier()
+    dist.destroy_process_group()
