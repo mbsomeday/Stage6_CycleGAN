a5820ff890e8499f6ec7cba453ab1c366555dbca
mbsomeday <137542994@qq.com>
2026-03-09 14:33:53 +0000

    update

--- options/test_options.py
+++ options/test_options.py
@@ -9,6 +9,11 @@ class TestOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
+
+        # 数据
+        parser.add_argument('--dataset_name_list', nargs='+', default=['D2', 'D1'], help='style transfer from A to B')
+        parser.add_argument('--data_key', type=str, default='Stage6_org')
+
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
--- scripts/CycleGAN_test.py
+++ scripts/CycleGAN_test.py
@@ -4,10 +4,11 @@ root_path = os.path.split(curPath)[0]
 sys.path.append(root_path)
 
 import torch
+from torch.utils.data import DataLoader
 
 from options.test_options import TestOptions
 from models.cycle_gan_model import CycleGANModel
-# from util.my_dataset import cycleGAN_Dataset
+from util.my_dataset import cycleGAN_Dataset
 
 
 opt = TestOptions().parse()  # get test options
@@ -15,8 +16,13 @@ opt.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(opt)
 
+# 加载模型
 cycleGAN = CycleGANModel(opt)
 cycleGAN.setup(opt)
+
+# 加载数据
+test_dataset = cycleGAN_Dataset(dataset_name_list=opt.dataset_name_list, path_key=opt.data_key, txt_name='test.txt')
+test_loader = DataLoader(test_dataset, batch_size=opt.test_batch_size, shuffle=False)
 
 
 
