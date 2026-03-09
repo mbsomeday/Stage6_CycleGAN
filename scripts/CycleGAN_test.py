f8b954d17238c90e50ae1dc4ba109f92effa4dcf
mbsomeday <137542994@qq.com>
2026-03-09 14:38:47 +0000

    update

--- scripts/CycleGAN_test.py
+++ scripts/CycleGAN_test.py
@@ -19,11 +19,20 @@ print(opt)
 # 加载模型
 cycleGAN = CycleGANModel(opt)
 cycleGAN.setup(opt)
+cycleGAN.eval()
 
 # 加载数据
 test_dataset = cycleGAN_Dataset(dataset_name_list=opt.dataset_name_list, path_key=opt.data_key, txt_name='test.txt')
 test_loader = DataLoader(test_dataset, batch_size=opt.test_batch_size, shuffle=False)
 
+
+for i, data in enumerate(test_loader):
+    cycleGAN.set_input(data)
+    ret = cycleGAN.test()
+    print(ret.shape)
+
+
+    break
 
 
 
