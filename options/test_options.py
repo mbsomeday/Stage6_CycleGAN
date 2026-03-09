aa4c90db3dce05ffec0be0ebf60e4da15ba97041
mbsomeday <137542994@qq.com>
2026-03-09 14:39:24 +0000

    update

--- options/test_options.py
+++ options/test_options.py
@@ -12,6 +12,7 @@ class TestOptions(BaseOptions):
 
         # 数据
         parser.add_argument('--dataset_name_list', nargs='+', default=['D2', 'D1'], help='style transfer from A to B')
+        parser.add_argument('--test_batch_size', type=int, default=32)
         parser.add_argument('--data_key', type=str, default='Stage6_org')
 
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
