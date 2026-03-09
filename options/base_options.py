670b9158993830564039f9c9cacc0b3811eefdd7
mbsomeday <137542994@qq.com>
2026-03-09 14:06:45 +0000

    update

--- options/base_options.py
+++ options/base_options.py
@@ -1,3 +1,9 @@
+# 将上级目录加入 sys.path， 防止命令行运行时找不到包
+import os, sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(curPath)[0]
+sys.path.append(root_path)
+
 import argparse
 from pathlib import Path
 from util import util
